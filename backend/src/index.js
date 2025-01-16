59d514431f2fff81d129148e3a16e75f919b96cd
jaykchen@icloud.com <jaykchen@icloud.com>
2025-01-16 10:53:15 -0500

    add more browser header can scrape reddit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,44 +1,54 @@
-import { chromium } from 'playwright'
+import { chromium } from "playwright";
 
-let browser
-const url = process.argv[2]
+let browser;
+const url = process.argv[2];
 
-async function initializeBrowser () {
+async function initializeBrowser() {
   try {
+    var agent =
+      "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0";
     browser = await chromium.launch({
-      executablePath: '/run/current-system/sw/bin/chromium',
+      executablePath: "/run/current-system/sw/bin/chromium",
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      ignoreHTTPSErrors: true
-    })
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-gpu",
+        "--disable-extensions",
+        `--user-agent=${agent}`,
+        "--window-size=768,1024",
+        "--force-device-scale-factor=1",
+      ],
+      ignoreHTTPSErrors: true,
+    });
   } catch (error) {
-    console.error('Failed to launch the browser:', error)
-    process.exit(1)
+    console.error("Failed to launch the browser:", error);
+    process.exit(1);
   }
 }
 
-async function validateAndParseUrl (inputUrl) {
+async function validateAndParseUrl(inputUrl) {
   try {
     const parsedUrl = new URL(
-      inputUrl.startsWith('http') ? inputUrl : `https://${inputUrl}`
-    )
-    return parsedUrl.href
+      inputUrl.startsWith("http") ? inputUrl : `https://${inputUrl}`
+    );
+    return parsedUrl.href;
   } catch {
-    throw new Error('Invalid URL format')
+    throw new Error("Invalid URL format");
   }
 }
 
-async function openOneTab (targetUrl) {
-  const page = await browser.newPage()
+async function openOneTab(targetUrl) {
+  const page = await browser.newPage();
   try {
-    const validUrl = await validateAndParseUrl(targetUrl)
+    const validUrl = await validateAndParseUrl(targetUrl);
     await page.goto(validUrl, {
-      waitUntil: 'domcontentloaded',
-      timeout: 30000
-    })
-    return await page.content()
+      waitUntil: "domcontentloaded",
+      timeout: 30000,
+    });
+    return await page.content();
   } finally {
-    await page.close()
+    await page.close();
   }
 }
 
@@ -46,17 +56,15 @@ async function openOneTab (targetUrl) {
 
 // const url = "https://agriculture.canada.ca/en/sector/animal-industry/red-meat-and-livestock-market-information/prices"; // Change as needed
 
-;(async () => {
-  await initializeBrowser() // Initialize the browser
-  const result = await openOneTab(url) // Open the tab and get the content
+(async () => {
+  await initializeBrowser(); // Initialize the browser
+  const result = await openOneTab(url); // Open the tab and get the content
 
   if (result) {
-    console.log(result)
+    console.log(result);
   } else {
-    console.log('Failed to extract the article.')
+    console.log("Failed to extract the article.");
   }
 
-  await browser.close()
-
-  return result
-})()
+  await browser.close();
+})();
